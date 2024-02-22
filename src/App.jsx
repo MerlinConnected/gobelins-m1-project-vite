381ea9d39d3c63c43f8bf3fc4be140b0290c11c2
Néo <miannayneo@gmail.com>
2024-02-22 14:35:25 +0100

    feat(model): added car model

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,22 @@
-import { Suspense, useRef, useState } from 'react';
+import { Suspense, useRef, useState, useEffect, createRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, Environment, Loader } from '@react-three/drei';
-import { Leva, useControls } from 'leva';
+import { OrbitControls, Environment, Loader, PerspectiveCamera } from '@react-three/drei';
 
 import UI from './components/UI';
-
-function Model() {
-  const meshRef = useRef();
-
-  const [hovered, hover] = useState(false);
-
-  const { color } = useControls({
-    color: {
-      value: '#FF69B4',
-      label: 'Color',
-      format: (v) => v,
-    },
-  });
-
-  useFrame(() => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += 0.01;
-      meshRef.current.rotation.y += 0.01;
-    }
-  });
-
-  return (
-    <mesh ref={meshRef} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} scale={hovered ? 2 : 1}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={color} roughness={0} />
-    </mesh>
-  );
-}
+import { Players } from './components/Players';
 
 export default function Scene() {
+  const ref = createRef();
+
+  console.log('ref.current', ref);
+
   return (
     <>
-      <Canvas shadows camera={{ position: [0, 10, 0] }}>
+      {/* <Canvas shadows camera={{ position: [2, 2, 2] }}> */}
+      <Canvas shadows camera={{ manual: true }}>
+        <PerspectiveCamera makeDefault position={[3, 3, 3]} />
         <Suspense fallback={null}>
-          <Model />
+          <Players ref={ref} />
           <OrbitControls target={(0, 0, 0)} />
           <ambientLight intensity={1} />
           <Environment preset="city" />
