f5d6e1bb80f12bd02ee927848074c520bc19576c
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-02-22 09:30:38 +0100

    chore: removed locks from gitignore
    fix(daronEngine): renamed folder to hooks
    
    fix(daronEngine): renamed folder to hooks

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-import { DaronEngineProvider } from './hook/useDaronEngine';
+import { DaronEngineProvider } from './hooks/useDaronEngine';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
