00ecf6f6472a37f3808c0aab275ecf97a8ca6d1b
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-03-15 12:16:04 +0100

    feat(Onboarding): styled onboarding component

--- src/blocks/onboarding/Onboarding.jsx
+++ src/blocks/onboarding/Onboarding.jsx
@@ -2,29 +2,39 @@ import React from 'react';
 
 import { useGameStateContext } from '../../provider/GameStateProvider';
 
-function Onboarding() {
-  const { setRoomCode, handleInsertCoin } = useGameStateContext();
+import classNames from 'classnames';
+import styles from './Onboarding.module.scss';
+
+import Button from '../../components/button/Button';
+
+function Onboarding({ className, ...props }) {
+  const { handleInsertCoin } = useGameStateContext();
 
   return (
-    <>
-      <button
-        onClick={() => {
-          handleInsertCoin();
-        }}
-      >
-        New room
-      </button>
-      <button
-        onClick={() => {
-          const roomCode = prompt('Enter room code');
-          if (roomCode) {
-            handleInsertCoin(roomCode);
-          }
-        }}
-      >
-        Join room
-      </button>
-    </>
+    <div className={classNames(styles.wrapper, className)} {...props}>
+      <div>
+        <h1>Onboarding</h1>
+        <div>
+          <Button
+            onClick={() => {
+              handleInsertCoin();
+            }}
+          >
+            New room
+          </Button>
+          <Button
+            onClick={() => {
+              const roomCode = prompt('Enter room code');
+              if (roomCode) {
+                handleInsertCoin(roomCode);
+              }
+            }}
+          >
+            Join room
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
