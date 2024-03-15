40bc0c34c90fe54212ff6a34f9d74b69b3c8acca
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-03-15 14:19:50 +0100

    feat(Lobby): styled component

--- src/blocks/lobby/Lobby.jsx
+++ src/blocks/lobby/Lobby.jsx
@@ -3,16 +3,22 @@ import React from 'react';
 import { usePlayersList, getRoomCode, isHost } from 'playroomkit';
 
 import classNames from 'classnames';
-
 import styles from './Lobby.module.scss';
+
+import Button from '../../components/button/Button';
 
 import { useGameStateContext } from '../../provider/GameStateProvider';
 import { GAME_PHASE } from '../../utils/constants';
+import { myPlayer } from 'playroomkit';
 
 function Lobby({ className, ...props }) {
   const { setLobby, setGlobalPhase } = useGameStateContext();
 
   const players = usePlayersList(true);
+
+  const me = myPlayer();
+
+  console.log(me);
 
   function copyRoomCode() {
     navigator.clipboard.writeText(getRoomCode());
@@ -26,33 +32,45 @@ function Lobby({ className, ...props }) {
   console.log(players);
 
   return (
-    <>
-      <div style={{ position: 'absolute', top: 0, zIndex: 1 }}>
-        <button
-          onClick={() => {
-            copyRoomCode();
-          }}
-        >
-          Copy room code
-        </button>
-        <button onClick={() => removeRoomHash()}>Leave room</button>
-
-        {players.map((player) => (
-          <p key={player.id}>{player.state.name || player.state.profile?.name}</p>
-        ))}
-
+    <div className={classNames(styles.wrapper, className)} {...props}>
+      <div>
+        <div>
+          <h1>Lobby</h1>
+          <div>
+            <Button
+              onClick={() => {
+                copyRoomCode();
+              }}
+            >
+              Invite friends
+            </Button>
+          </div>
+        </div>
+        <div>
+          <p>Players :</p>
+          {players.map((player) => (
+            <h2 key={player.id} className={classNames({ [styles.me]: me?.id === player?.id })}>
+              {player.state.name || player.state.profile?.name}
+            </h2>
+          ))}
+        </div>
         {isHost() && (
-          <button
+          <Button
+            className={styles.white}
             onClick={() => {
               setLobby(false);
               setGlobalPhase(GAME_PHASE.startGame, true);
             }}
           >
             Start game
-          </button>
+          </Button>
         )}
+        {!isHost() && <em>Waiting for host to start</em>}
       </div>
-    </>
+      <Button className={styles.leave} onClick={() => removeRoomHash()}>
+        Leave room
+      </Button>
+    </div>
   );
 }
 
