6b5ecad6675ff56f9f6af069523231084968aa9f
Néo <miannayneo@gmail.com>
2024-04-09 12:03:51 +0200

    Feat(Player) camera offset to local model

--- src/blocks/player/Player.jsx
+++ src/blocks/player/Player.jsx
@@ -9,30 +9,50 @@ import { Model } from '../../models/car';
 import Billboard from '../../components/billboard/Billboard';
 
 import paths from '../../utils/paths.json';
+import { useEffect } from 'react';
 
 function Player({ player, index, ...props }) {
-  const { position } = props;
+  const ref = React.useRef();
+  const { position, rotationY } = props;
 
   const { id, state } = player;
   const me = myPlayer();
-
-  console.log(paths.players);
+  const camRef = React.useRef();
 
   const [points] = usePlayerState(player, 'points');
 
   const cameraPos = useMemo(() => {
-    const pos = new Vector3(10, 8, 20);
+    const pos = new Vector3(position[0], position[1], position[2]);
     return pos;
-  }, []);
+  }, [me]);
+
+  useEffect(() => {
+    if (me?.id === id) {
+      camRef.current.position.set(cameraPos.x, cameraPos.y, cameraPos.z);
+      // camRef.current.lookAt(0, 0, 0);
+
+      let tempVec = new Vector3();
+
+      ref.current.getWorldDirection(tempVec);
+
+      camRef.current.position.addScaledVector(tempVec, -5);
+      tempVec.cross(new Vector3(0, 1, 0)).normalize();
+      camRef.current.position.addScaledVector(tempVec, 2);
+      camRef.current.position.y += 2;
+
+      // camera.position.sub
+      console.log(camRef.current.position);
+    }
+  }, [player]);
 
   return (
-    <group {...props}>
-      <group>
+    <>
+      <group ref={ref} rotation-y={rotationY} {...props}>
         <Billboard player={player} position={[0, 2, 0]} />
         <Model color={state?.profile?.color} />
       </group>
-      {me?.id === id && <PerspectiveCamera makeDefault position={cameraPos} />}
-    </group>
+      {me?.id === id && <PerspectiveCamera ref={camRef} makeDefault />}
+    </>
   );
 }
 
