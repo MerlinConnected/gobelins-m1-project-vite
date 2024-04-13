c1c41d9988de3737a0718cf16f82e8cb4bbd92ea
Néo <miannayneo@gmail.com>
2024-04-13 12:36:42 +0200

    Feat(Player) : curve creation according to points earned

--- src/blocks/player/Player.jsx
+++ src/blocks/player/Player.jsx
@@ -1,49 +1,50 @@
-import React, { useMemo, useRef } from 'react';
-
+import React, { useMemo, useRef, useEffect, useState } from 'react';
+import { useFrame, useThree } from '@react-three/fiber';
 import { PerspectiveCamera } from '@react-three/drei';
-import { Vector3 } from 'three';
+import * as THREE from 'three';
 
 import { myPlayer, usePlayerState } from 'playroomkit';
+import { animate } from 'framer-motion';
+
+import Billboard from '../../components/billboard/Billboard';
+import Path from '../../utils/paths';
+import getCurveFromPlayer from '../../utils/getCurveFromPlayer';
 
 import { Model } from '../../models/car';
 
-import Billboard from '../../components/billboard/Billboard';
-
-import paths from '../../utils/paths.json';
-import { useEffect } from 'react';
-import { useState } from 'react';
-
 function Player({ player, index, ...props }) {
-  const ref = React.useRef();
   const { position, rotationY } = props;
-
-  const [prevPoints, setPrevPoints] = useState(0);
-
   const { id, state } = player;
+
+  const ref = useRef();
+  const camRef = useRef();
+  const progress = useRef(0);
+  const [prevPoints, setPrevPoints] = useState(0);
+  const { scene } = useThree();
+
   const me = myPlayer();
-  const camRef = React.useRef();
-
-  const path = paths.players[index];
-
   const [points] = usePlayerState(player, 'points');
 
   const cameraPos = useMemo(() => {
-    const pos = new Vector3(position[0], position[1], position[2]);
+    const pos = new THREE.Vector3(position[0], position[1], position[2]);
     return pos;
   }, [player]);
 
+  let curve = useRef(null);
+
   useEffect(() => {
     if (me?.id === id) {
+      // set the camera pos next to the player
       camRef.current.position.copy(cameraPos);
 
-      let tempVec = new Vector3();
+      let tempVec = new THREE.Vector3();
 
       ref.current.getWorldDirection(tempVec);
 
-      camRef.current.position.addScaledVector(tempVec, -5);
-      tempVec.cross(new Vector3(0, 1, 0)).normalize();
+      camRef.current.position.addScaledVector(tempVec, -20);
+      tempVec.cross(new THREE.Vector3(0, 1, 0)).normalize();
       camRef.current.position.addScaledVector(tempVec, 2);
-      camRef.current.position.y += 2;
+      camRef.current.position.y += 3;
     }
   }, [player]);
 
@@ -52,9 +53,28 @@ function Player({ player, index, ...props }) {
     let tilesToMove = currentPoints - prevPoints;
     setPrevPoints(currentPoints);
 
-    console.log('tilesToMove', tilesToMove, 'prevPoints', prevPoints);
+    if (tilesToMove > 0 && currentPoints < Path[index].length) {
+      const newPointsArray = Path[index].slice(prevPoints, prevPoints + tilesToMove);
 
-    const startingPoint = path[prevPoints - 1];
+      const playerPos = ref.current.position.clone();
+
+      curve.current = getCurveFromPlayer(playerPos, newPointsArray);
+
+      const geometry = new THREE.TubeGeometry(curve.current, 200, 0.1, 8, false);
+      const mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ color: 0xff0000 }));
+      scene.add(mesh);
+
+      animate(progress.current, 1, {
+        duration: 2,
+        onUpdate: (value) => {
+          let pos = curve.current.getPointAt(value);
+          ref.current.position.copy(pos);
+          let tangent = curve.current.getTangentAt(value).normalize();
+          pos.add(tangent);
+          ref.current.lookAt(pos);
+        },
+      });
+    }
   }, [points]);
 
   return (
---
+++ src/utils/paths.jsx
@@ -0,0 +1,59 @@
+import * as THREE from 'three';
+
+const Path1 = [
+  new THREE.Vector3(0, 0, 4),
+  new THREE.Vector3(-1.5, 0, 1.5),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(1, 0, 0),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-0.5, 0, -0.5),
+  new THREE.Vector3(0, 0, -1),
+  new THREE.Vector3(0, 0, -1),
+  new THREE.Vector3(-0.5, 0, -0.5),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(1, 0, 0),
+  new THREE.Vector3(1, 0, 0),
+];
+
+const Path2 = [
+  new THREE.Vector3(0, 0, 4),
+  new THREE.Vector3(-1.5, 0, 1.5),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(1, 0, 0),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-0.5, 0, -0.5),
+  new THREE.Vector3(0, 0, -1),
+  new THREE.Vector3(0, 0, -1),
+  new THREE.Vector3(-0.5, 0, -0.5),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(1, 0, 0),
+  new THREE.Vector3(1, 0, 0),
+];
+
+const Path = [Path1, Path2];
+
+export default Path;
