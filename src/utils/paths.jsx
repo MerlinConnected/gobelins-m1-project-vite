04a33e283f33ff29f323eb64246b83e700af84b2
Gaëtan Jestin <gaetanjestin@outlook.com>
2024-05-12 23:02:26 +0200

    feat: this is the broken code

--- src/utils/paths.jsx
+++ src/utils/paths.jsx
@@ -50,6 +50,56 @@ const Path2 = [
   new THREE.Vector3(1, 0, 0),
 ];
 
-const Path = [Path1, Path2];
+const Path3 = [
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(1, 0, 0),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(1, 0, 0),
+  new THREE.Vector3(0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(0, 0, 1),
+
+  new THREE.Vector3(0, 0, 1),
+];
+
+const Path4 = [
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-0.5, 0, -0.5),
+  new THREE.Vector3(0, 0, -1),
+  new THREE.Vector3(-0.5, 0, -0.5),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(0, 0, 1),
+  new THREE.Vector3(-0.5, 0, 0.5),
+  new THREE.Vector3(-1, 0, 0),
+  new THREE.Vector3(-0.5, 0, -0.5),
+  new THREE.Vector3(0, 0, -1),
+  new THREE.Vector3(-0.5, 0, -0.5),
+  new THREE.Vector3(-1, 0, 0),
+
+  new THREE.Vector3(-1, 0, 0),
+];
+
+const Path = [Path1, Path2, Path3, Path4];
 
 export default Path;
