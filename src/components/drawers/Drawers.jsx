c8b4714020e208c7381f40471a06dbdbd83d422a
Néo <miannayneo@gmail.com>
2024-05-23 10:16:19 +0200

    Fix drawer animation

--- src/components/drawers/Drawers.jsx
+++ src/components/drawers/Drawers.jsx
@@ -8,10 +8,19 @@ import Drawer from '../drawer/Drawer';
 import StrokeText from '../stroke-text/StrokeText';
 
 function Drawers({ className, drawersDisabled, ...props }) {
-  const [drawnCards, setDrawnCards] = useState(0);
+  const [drawnCardTypes, setDrawnCardTypes] = useState([]);
 
-  const handleDrawnCards = () => {
-    setDrawnCards((prevCount) => prevCount + 1);
+  const handleDrawnCards = (type) => {
+    setDrawnCardTypes((prevTypes) => [...prevTypes, type]);
+  };
+
+  const getColor = (index) => {
+    if (index < drawnCardTypes.length) {
+      return drawnCardTypes[index] === 'transport'
+        ? 'var(--color-drawer-light-transport)'
+        : 'var(--color-drawer-light-action)';
+    }
+    return 'none';
   };
 
   return (
@@ -33,27 +42,28 @@ function Drawers({ className, drawersDisabled, ...props }) {
         <div className={styles.drawersHelper}>
           <StrokeText medium>Choisis bien tes cartes</StrokeText>
           <div className={styles.checkpoints}>
-            <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M24.0073 3L33 12.555L31.087 26.6367L21.5203 33L8.97071 32.0214L3 20.4674L6.78748 9.12243L12.5274 3H24.0073Z"
-                fill={drawnCards >= 1 ? '#A8EEE5' : 'none'}
-                stroke="white"
-                strokeWidth="4.5"
-              />
-            </svg>
-            <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M24.0073 3L33 12.555L31.087 26.6367L21.5203 33L8.97071 32.0214L3 20.4674L6.78748 9.12243L12.5274 3H24.0073Z"
-                fill={drawnCards >= 2 ? '#A8EEE5' : 'none'}
-                stroke="white"
-                strokeWidth="4.5"
-              />
-            </svg>
+            {Array.from({ length: 2 }).map((_, index) => (
+              <svg
+                key={index}
+                width="36"
+                height="36"
+                viewBox="0 0 36 36"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M24.0073 3L33 12.555L31.087 26.6367L21.5203 33L8.97071 32.0214L3 20.4674L6.78748 9.12243L12.5274 3H24.0073Z"
+                  fill={getColor(index)}
+                  stroke="white"
+                  strokeWidth="4.5"
+                />
+              </svg>
+            ))}
           </div>
         </div>
         <div className={styles.drawers}>
-          <Drawer type="transport" handleDrawnCards={handleDrawnCards} />
-          <Drawer type="action" handleDrawnCards={handleDrawnCards} />
+          <Drawer type="transport" handleDrawnCards={() => handleDrawnCards('transport')} />
+          <Drawer type="action" handleDrawnCards={() => handleDrawnCards('action')} />
         </div>
       </motion.div>
     )
